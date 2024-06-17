7314ff5509e41ad291457410964bdcfa81d2b04d
Danny Buglak <dannybuglak4@gmail.com>
2024-06-17 14:09:21 -0400

    Project set up fix

---
+++ hello-people/src/main/java/com/pluralsight/hello_people/services/PeopleService.java
@@ -0,0 +1,38 @@
+package com.pluralsight.hello_people.services;
+
+import com.pluralsight.hello_people.models.Person;
+import com.pluralsight.hello_people.repositories.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PeopleService implements DataService<Person> {
+    private @Autowired PeopleRepository repository;
+
+    @Override
+    public List<Person> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public Person get(int index) {
+        return repository.get(index);
+    }
+
+    @Override
+    public int create(Person item) {
+        return repository.create(item);
+    }
+
+    @Override
+    public boolean update(Person item) {
+        return repository.update(item);
+    }
+
+    @Override
+    public boolean delete(Person item) {
+        return repository.delete(item);
+    }
+}
